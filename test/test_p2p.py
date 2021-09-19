d4fe3316f8e19b59d4614624734aac20ce6a2ba0
Adrià Quintanas-Corominas <adria.quintanas@bsc.es>
2021-09-19 18:05:32 +0200

    Add test for the files filter

--- test/test_p2p.py
+++ test/test_p2p.py
@@ -2,7 +2,7 @@
 import sys
 import datetime
 import pytest
-from p2p import get_latest_report_date
+from p2p import get_latest_report_date, filter_investment_files_by_newest_date
 
 def test_get_latest_report_date():
     input_data = {
@@ -14,6 +14,30 @@ def test_get_latest_report_date():
         datetime.date.fromisoformat("2021-01-01") : "path6.xls",
     }
     assert datetime.date.fromisoformat("2021-12-01") == get_latest_report_date(input_data)
+
+
+def test_filter_investment_files_by_newest_date():
+    input_data = {
+        'platform_A' : {
+            datetime.date.fromisoformat("2021-11-01") : "path1.xls",
+            datetime.date.fromisoformat("2021-12-01") : "path2.xls",
+        },
+        'platform_B' : {
+            datetime.date.fromisoformat("2021-11-01") : "path1.xls",
+            datetime.date.fromisoformat("2021-12-01") : "path2.xls",
+            datetime.date.fromisoformat("2022-01-01") : "path3.xls",
+        },
+    }
+    output_data = {
+        'platform_A' : {
+            datetime.date.fromisoformat("2021-12-01") : "path2.xls",
+        },
+        'platform_B' : {
+            datetime.date.fromisoformat("2022-01-01") : "path3.xls",
+        },
+    }
+    assert output_data == filter_investment_files_by_newest_date(input_data)
+
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__]))
