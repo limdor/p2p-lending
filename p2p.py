d5478ff140b51c351f026121e113213785c0ecde
Xavier Bonaventura <xavibonaventura@gmail.com>
2021-10-12 23:12:19 +0200

    Separate diversification report generation from report display

--- p2p.py
+++ p2p.py
@@ -192,40 +192,43 @@ def report(show_past_investments):
             'NumberLoanParts' : total_invested_parts
         }
 
+    diversification_report_per_date = {}
+    for date, overall_data in overall_report.items():
+        diversification_report_per_date[date] = {}
+        diversification_report_per_date[date]['reportId'] = date
+
+        # Overall statistics
+        total_invested_parts = overall_data['TotalInvestment']
+        diversification_report_per_date[date]['overallInvestment'] = total_invested_parts
+        total_loan_parts = overall_data['NumberLoanParts']
+        diversification_report_per_date[date]['loanParts'] = total_loan_parts
+
+        # Statistics by Country
+        overall_group_by_country = overall_data['DataByCountry']
+        sum_on_top_3_countries = overall_group_by_country[OUTSTANDING_PRINCIPAL][0:3].sum()
+        percentage_top_3_countries = (sum_on_top_3_countries / total_invested_by_date) * 100
+        top_country = overall_group_by_country[OUTSTANDING_PRINCIPAL][0]
+        percentage_top_country = (top_country / total_invested_by_date) * 100
+        diversification_report_per_date[date]['countryStatistics'] = {'investmentOneCountry':percentage_top_country, 'investmentThreeCountries': percentage_top_3_countries}
+
+        # Statistics by Loan Originator
+        overall_group_by_originator = overall_data['DataByLoanOriginator']
+        sum_on_top_5_originators = overall_group_by_originator[OUTSTANDING_PRINCIPAL][0:5].sum()
+        percentage_top_5_originators = (sum_on_top_5_originators / total_invested_by_date) * 100
+        top_originator = overall_group_by_originator[OUTSTANDING_PRINCIPAL][0]
+        percentage_top_originator = (top_originator / total_invested_by_date) * 100
+        diversification_report_per_date[date]['originatorStatistics'] = {'investmentOneOriginator':percentage_top_originator, 'investmentFiveOriginators': percentage_top_5_originators}
 
     logger.info("*********************************")
     logger.info("**** Diversification reports ****")
     logger.info("*********************************")
-    for date, overall_data in overall_report.items():
+    for date, report in diversification_report_per_date.items():
         logger.info(f"Investments diversification on {date}")
-
-        # Overall statistics
-        total_invested_parts = overall_data['TotalInvestment']
-        logger.info(f"|- Diversification Investment: {total_invested_parts:.2f}€")
-
-        total_loan_parts = overall_data['NumberLoanParts']
-        logger.info(f'|- The portfolio consists of at least 100 different loan parts: {total_loan_parts:d}')
-
-        # Statistics by Country
+        logger.info(f"|- Diversification Investment: {report['overallInvestment']:.2f}€")
+        logger.info(f"|- The portfolio consists of at least 100 different loan parts: {report['loanParts']:d}")
         logger.info(f"|- Statistics by Country:")
-        overall_group_by_country = overall_data['DataByCountry']
-
-        sum_on_top_3_countries = overall_group_by_country[OUTSTANDING_PRINCIPAL][0:3].sum()
-        percentage_top_3_countries = (sum_on_top_3_countries / total_invested_by_date) * 100
-        logger.info(f'   |- No more than 50% of loans are issued in 3 (or less) countries: {percentage_top_3_countries:.2f}%')
-
-        top_country = overall_group_by_country[OUTSTANDING_PRINCIPAL][0]
-        percentage_top_country = (top_country / total_invested_by_date) * 100
-        logger.info(f'   |- No more than 33% of loans are issued in any single country: {percentage_top_country:.2f}%')
-
-        # Statistics by Loan Originator
+        logger.info(f"   |- No more than 50% of loans are issued in 3 (or less) countries: {report['countryStatistics']['investmentThreeCountries']:.2f}%")
+        logger.info(f"   |- No more than 33% of loans are issued in any single country: {report['countryStatistics']['investmentOneCountry']:.2f}%")
         logger.info(f"|- Statistics by Originator:")
-        overall_group_by_originator = overall_data['DataByLoanOriginator']
-
-        sum_on_top_5_originators = overall_group_by_originator[OUTSTANDING_PRINCIPAL][0:5].sum()
-        percentage_top_5_originators = (sum_on_top_5_originators / total_invested_by_date) * 100
-        logger.info(f'   |- No more than 50% of loans are issued by 5 (or less) lending companies: {percentage_top_5_originators:.2f}%')
-
-        top_originator = overall_group_by_originator[OUTSTANDING_PRINCIPAL][0]
-        percentage_top_originator = (top_originator / total_invested_by_date) * 100
-        logger.info(f'   |- No more than 20% of loans are issued by any single lending company: {percentage_top_originator:.2f}%')
+        logger.info(f"   |- No more than 50% of loans are issued by 5 (or less) lending companies: {report['originatorStatistics']['investmentFiveOriginators']:.2f}%")
+        logger.info(f"   |- No more than 20% of loans are issued by any single lending company: {report['originatorStatistics']['investmentOneOriginator']:.2f}%")
