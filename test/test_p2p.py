f5e65266bf462ac87d37e09b805a72473623044a
Xavier Bonaventura <xavibonaventura@gmail.com>
2021-10-23 21:08:28 +0200

    Add test for diversification report generation

--- test/test_p2p.py
+++ test/test_p2p.py
@@ -1,8 +1,10 @@
 import datetime
 from unittest.mock import patch
+import pytest
 import pandas
 from p2p import get_latest_report_date, filter_investment_files_by_newest_date, \
-    read_marketplace_files, generate_overall_report_per_date
+    read_marketplace_files, generate_overall_report_per_date, \
+    generate_diversification_report_per_date
 
 
 def test_get_latest_report_date():
@@ -159,3 +161,39 @@ def test_generate_overall_report_per_date():
                     check_names=False, check_index_type=False, check_dtype=False, atol=1e-3)
             else:
                 assert value == expected_overall_report_per_date[date][key]
+
+def test_generate_diversification_report_per_date():
+    input_overall_report = {
+        datetime.date(2020, 11, 30): {
+            'DataByCountry': pandas.DataFrame({
+                'Outstanding principal': {'Spain': 150.0, 'Poland': 75.0},
+                'Percentage': {'Spain': 0.666, 'Poland': 0.333}
+            }),
+            'DataByLoanOriginator': pandas.DataFrame({
+                'Outstanding principal': {
+                    'Creamfinance': 100.0, 'Sun Finance': 100.0, 'Creditstar': 25.0
+                },
+                'Percentage': {
+                    'Creamfinance': 0.444, 'Sun Finance': 0.444, 'Creditstar': 0.111
+                }
+            }),
+            'TotalInvestment': 225.0,
+            'NumberLoanParts': 5
+        },
+    }
+    expected_output = {
+        datetime.date(2020, 11, 30): {
+            'reportId': datetime.date(2020, 11, 30),
+            'countryStatistics': {
+                'investmentOneCountry': pytest.approx(66.66, abs=1e-2),
+                'investmentThreeCountries': 100.0
+            },
+            'originatorStatistics': {
+                'investmentOneOriginator': pytest.approx(44.44, abs=1e-2),
+                'investmentFiveOriginators': 100.0
+            },
+            'overallInvestment': 225.0,
+            'loanParts': 5,
+        }
+    }
+    assert expected_output == generate_diversification_report_per_date(input_overall_report)
