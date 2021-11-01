64cc49499363b38627d4fd298abbf12ce3bd1be8
Adrià Quintanas-Corominas <adria.quintanas@bsc.es>
2021-11-01 20:32:33 +0100

    create test for the overall reports

---
+++ test/test_overall_reports.py
@@ -0,0 +1,78 @@
+import datetime
+from io import StringIO
+import pandas
+from reports import overall
+
+def test_overall_generate_report_per_date():
+    input_data = StringIO(
+        "Country,Loan originator,Outstanding principal,Investment platform,Date\n"\
+        "Poland,Sun Finance,50.0,mintos,2020-11-30\n"\
+        "Spain,Creamfinance,75.0,mintos,2020-11-30\n"\
+        "Poland,Creditstar,10.0,mintos,2020-11-30\n"\
+        "Spain,Creamfinance,75.0,mintos,2020-11-30\n"\
+        "Poland,Creditstar,15.0,mintos,2020-11-30\n"
+    )
+    input_data_frame = pandas.read_csv(input_data, parse_dates=['Date'])
+
+    input_files = {
+        'mintos': {
+            datetime.date(2021, 6, 30): './data/mintos/20210630-current-investments.xlsx',
+            datetime.date(2020, 11, 30): './data/mintos/20201130-current-investments.xlsx'
+        },
+        'iuvo': {
+            datetime.date(2020, 11, 30): './data/iuvo/MyInvestments-20201130.xlsx',
+            datetime.date(2021, 6, 30): './data/iuvo/MyInvestments-20210630.xlsx'
+        }
+    }
+    overall_report_per_date = overall.generate_report_per_date(input_data_frame, input_files)
+
+    expected_overall_report_per_date = {
+        datetime.date(2020, 11, 30): {
+            'Data': input_data_frame,
+            'DataByCountry': pandas.DataFrame({
+                'Outstanding principal': {'Spain': 150.0, 'Poland': 75.0},
+                'Percentage': {'Spain': 0.666, 'Poland': 0.333}
+            }),
+            'DataByLoanOriginator': pandas.DataFrame({
+                'Outstanding principal': {
+                    'Creamfinance': 150.0, 'Sun Finance': 50.0, 'Creditstar': 25.0
+                },
+                'Percentage': {
+                    'Creamfinance': 0.666, 'Sun Finance': 0.222, 'Creditstar': 0.111
+                }
+            }),
+            'TotalInvestment': 225.0,
+            'NumberLoanParts': 5
+        },
+        datetime.date(2021, 6, 30): {
+            'Data': pandas.DataFrame({
+                'Country': {},
+                'Loan originator': {},
+                'Outstanding principal': {},
+                'Investment platform': {},
+                'Date': {}
+            }),
+            'DataByCountry': pandas.DataFrame({
+                'Outstanding principal': {},
+                'Percentage': {}
+            }),
+            'DataByLoanOriginator': pandas.DataFrame({
+                'Outstanding principal': {},
+                'Percentage': {}
+            }),
+            'TotalInvestment': 0,
+            'NumberLoanParts': 0
+        }
+    }
+
+    assert sorted(overall_report_per_date.keys()) == sorted(expected_overall_report_per_date.keys())
+    for date, overall_report in overall_report_per_date.items():
+        assert sorted(overall_report.keys()) == \
+            sorted(expected_overall_report_per_date[date].keys())
+        for key, value in overall_report.items():
+            if isinstance(value, pandas.core.frame.DataFrame):
+                pandas.testing.assert_frame_equal(
+                    value, expected_overall_report_per_date[date][key],
+                    check_names=False, check_index_type=False, check_dtype=False, atol=1e-3)
+            else:
+                assert value == expected_overall_report_per_date[date][key]
