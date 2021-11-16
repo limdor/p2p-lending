f5ded84e5474f2840944a9be8ad29747e9939c2e
Adrià Quintanas-Corominas <adria.quintanas@bsc.es>
2021-11-16 20:27:30 +0100

    split iterator and computation

--- reports/diversification.py
+++ reports/diversification.py
@@ -5,33 +5,39 @@ from marketplace import marketplace
 def generate_report_per_date(overall_report):
     diversification_report_per_date = {}
     for date, overall_data in overall_report.items():
-        diversification_report_per_date[date] = {}
-        diversification_report_per_date[date]['reportId'] = date
-
-        # Overall statistics
-        total_invested_parts = overall_data['TotalInvestment']
-        diversification_report_per_date[date]['overallInvestment'] = total_invested_parts
-        diversification_report_per_date[date]['loanParts'] = overall_data['NumberLoanParts']
-
-        # Statistics by Country
-        overall_group_by_country = overall_data['DataByCountry']
-        sum_on_top_3_countries = overall_group_by_country[marketplace.OUTSTANDING_PRINCIPAL][0:3].sum()
-        percentage_top_3_countries = (sum_on_top_3_countries / total_invested_parts) * 100
-        top_country = overall_group_by_country[marketplace.OUTSTANDING_PRINCIPAL][0]
-        percentage_top_country = (top_country / total_invested_parts) * 100
-        diversification_report_per_date[date]['countryStatistics'] = {
-            'investmentOneCountry': percentage_top_country, 'investmentThreeCountries': percentage_top_3_countries}
-
-        # Statistics by Loan Originator
-        overall_group_by_originator = overall_data['DataByLoanOriginator']
-        sum_on_top_5_originators = overall_group_by_originator[marketplace.OUTSTANDING_PRINCIPAL][0:5].sum()
-        percentage_top_5_originators = (sum_on_top_5_originators / total_invested_parts) * 100
-        top_originator = overall_group_by_originator[marketplace.OUTSTANDING_PRINCIPAL][0]
-        percentage_top_originator = (top_originator / total_invested_parts) * 100
-        diversification_report_per_date[date]['originatorStatistics'] = {
-            'investmentOneOriginator': percentage_top_originator, 'investmentFiveOriginators': percentage_top_5_originators}
+        diversification_report_per_date[date] = generate_report(date, overall_data)
 
     return diversification_report_per_date
+
+
+def generate_report(date, overall_data):
+    diversification_report = {}
+    diversification_report['reportId'] = date
+
+    # Overall statistics
+    total_invested_parts = overall_data['TotalInvestment']
+    diversification_report['overallInvestment'] = total_invested_parts
+    diversification_report['loanParts'] = overall_data['NumberLoanParts']
+
+    # Statistics by Country
+    overall_group_by_country = overall_data['DataByCountry']
+    sum_on_top_3_countries = overall_group_by_country[marketplace.OUTSTANDING_PRINCIPAL][0:3].sum()
+    percentage_top_3_countries = (sum_on_top_3_countries / total_invested_parts) * 100
+    top_country = overall_group_by_country[marketplace.OUTSTANDING_PRINCIPAL][0]
+    percentage_top_country = (top_country / total_invested_parts) * 100
+    diversification_report['countryStatistics'] = {
+        'investmentOneCountry': percentage_top_country, 'investmentThreeCountries': percentage_top_3_countries}
+
+    # Statistics by Loan Originator
+    overall_group_by_originator = overall_data['DataByLoanOriginator']
+    sum_on_top_5_originators = overall_group_by_originator[marketplace.OUTSTANDING_PRINCIPAL][0:5].sum()
+    percentage_top_5_originators = (sum_on_top_5_originators / total_invested_parts) * 100
+    top_originator = overall_group_by_originator[marketplace.OUTSTANDING_PRINCIPAL][0]
+    percentage_top_originator = (top_originator / total_invested_parts) * 100
+    diversification_report['originatorStatistics'] = {
+        'investmentOneOriginator': percentage_top_originator, 'investmentFiveOriginators': percentage_top_5_originators}
+
+    return diversification_report
 
 
 def print_report_per_date(diversification_report_per_date):
