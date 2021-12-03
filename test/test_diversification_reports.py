282176b79d98e795597f25b1a50e6eedaf9b5108
Adrià Quintanas-Corominas <adria.quintanas@bsc.es>
2021-12-03 18:23:13 +0100

    modify test to generate the report employing the input data frame

--- test/test_diversification_reports.py
+++ test/test_diversification_reports.py
@@ -16,25 +16,6 @@ def test_diversification_generate_report_per_date():
 
     input_data_frame = pandas.read_csv(input_data, parse_dates=['Date'])
 
-    input_overall_report = {
-        datetime.date(2020, 11, 30): {
-            'Data': input_data_frame,
-            'DataByCountry': pandas.DataFrame({
-                'Outstanding principal': {'Spain': 150.0, 'Poland': 75.0},
-                'Percentage': {'Spain': 0.666, 'Poland': 0.333}
-            }),
-            'DataByLoanOriginator': pandas.DataFrame({
-                'Outstanding principal': {
-                    'Creamfinance': 150.0, 'Sun Finance': 50.0, 'Creditstar': 25.0
-                },
-                'Percentage': {
-                    'Creamfinance': 0.666, 'Sun Finance': 0.222, 'Creditstar': 0.111
-                }
-            }),
-            'TotalInvestment': 225.0,
-            'NumberLoanParts': 5
-        },
-    }
     expected_output = {
         datetime.date(2020, 11, 30): {
             'reportId': datetime.date(2020, 11, 30),
@@ -50,4 +31,4 @@ def test_diversification_generate_report_per_date():
             'loanParts': 5,
         }
     }
-    assert expected_output == diversification.generate_report_per_date(input_overall_report)
+    assert expected_output == diversification.generate_report_per_date(input_data_frame)
