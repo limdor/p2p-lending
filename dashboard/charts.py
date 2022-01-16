0a3bf6e9b5447f1cf0b5aad98381a5a469aff6cc
Xavier Bonaventura <xavibonaventura@gmail.com>
2022-01-16 20:29:22 +0100

    Remove duplicated code

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -2,31 +2,30 @@ import plotly.express
 from marketplace import marketplace
 
 
-def piechart_PlatformOriginator(investment_raw_data):
+def piechart(investment_raw_data, group_by):
     investment_grouped_data = investment_raw_data.groupby(
-        [marketplace.INVESTMENT_PLATFORM, marketplace.LOAN_ORIGINATOR],
+        group_by,
         as_index=False).sum()
     fig = plotly.express.sunburst(
         investment_grouped_data,
-        path=[marketplace.INVESTMENT_PLATFORM, marketplace.LOAN_ORIGINATOR],
+        path=group_by,
         values='Outstanding principal',
         labels='')
     fig.update_traces(textinfo="label+percent entry")
     fig.update_layout(grid=dict(columns=1, rows=1),
         margin=dict(t=0, l=0, r=0, b=0))
     return fig
+
+
+def piechart_PlatformOriginator(investment_raw_data):
+    return piechart(
+        investment_raw_data,
+        [marketplace.INVESTMENT_PLATFORM, marketplace.LOAN_ORIGINATOR]
+    )
 
 
 def piechart_CountryPlatformOriginator(investment_raw_data):
-    investment_grouped_data = investment_raw_data.groupby(
-        [marketplace.COUNTRY, marketplace.INVESTMENT_PLATFORM, marketplace.LOAN_ORIGINATOR],
-        as_index=False).sum()
-    fig = plotly.express.sunburst(
-        investment_grouped_data,
-        path=[marketplace.COUNTRY, marketplace.INVESTMENT_PLATFORM, marketplace.LOAN_ORIGINATOR],
-        values='Outstanding principal',
-        labels='')
-    fig.update_traces(textinfo="label+percent entry")
-    fig.update_layout(grid=dict(columns=1, rows=1),
-        margin=dict(t=0, l=0, r=0, b=0))
-    return fig
+    return piechart(
+        investment_raw_data,
+        [marketplace.COUNTRY, marketplace.INVESTMENT_PLATFORM, marketplace.LOAN_ORIGINATOR]
+    )
