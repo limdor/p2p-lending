dffcef2a9d776471c6ee2951f7a273bccb79eb6f
Adrià Quintanas-Corominas <adria.quintanas@bsc.es>
2022-01-23 13:53:16 +0100

    modify frontend to add tabs

--- dashboard/components.py
+++ dashboard/components.py
@@ -2,7 +2,28 @@
 import dash
 import figures
 
-def add_figure_card(figure_id):
+def add_figure_card_full_row(figure_id):
+    return dash.html.Div(
+        [
+            dash.dcc.Graph(
+                    id=figure_id,
+                    figure=figures.blank(),
+                    className="border",
+                    style={
+                        'height': '80vh',
+                        'text-align': 'center',
+                        'margin': '10px',
+                        'box-shadow': 'rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
+                    }
+                )
+        ],
+        className="col",
+        style={
+            'height': 'fit-content',
+            },
+        )
+
+def add_figure_card_half_row(figure_id):
     return dash.html.Div(
         [
             dash.dcc.Graph(
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -23,6 +23,7 @@ external_stylesheets = [
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'p2p-lending'
+app.config.suppress_callback_exceptions = True
 
 app.layout = dash.html.Div(
     [
@@ -32,57 +33,41 @@ app.layout = dash.html.Div(
                 dash.html.Div(
                     [
                         dash.html.H1(
-                            'P2P-LENDING - Monthly Snapshot',
-                            style={'color':'#4b4b4b'},
+                            'P2P-LENDING',
+                            style={
+                                'color':'#4b4b4b',
+                                'height': '10vh',
+                                'lineHeight': '10vh',
+                                },
                         ),
                     ],
                     className='header',
                     style={
-                        'border-bottom': '1px solid #eee',
-                        'text-align': 'center'
+                        'textAlign': 'center',
                         },
                     ),
                 dash.html.Div(
-                    [
-                        dash.dcc.Upload(
-                            id='datatable-upload',
-                            children=dash.html.Div(
-                                [
-                                    dash.html.A('Drop/Upload Files')
-                                ]),
-                            style={
-                                'width': '100%',
-                                'height': '40px',
-                                'textAlign': 'center',
-                                'lineHeight': '40px',
-                                'margin': '10px',
-                                'box-shadow': 'rgba(3, 102, 214, 0.3) 0px 0px 0px 3px',
-                                },
-                            multiple=True
-                        ),
-                        dash.dash_table.DataTable(id='datatable-upload-container'),
-                    ],
-                    ),
-                dash.html.Div(
-                    [
-                        components.add_figure_card('piechart-CounyryPlatformOriginator'),
-                        components.add_figure_card('table-DataByCountry'),
-                    ],
-                    className="row row-cols-1 row-cols-md-2 row-cols-lg-2",
-                    ),
-                dash.html.Div(
-                    [
-                        components.add_figure_card('piechart-PlatformOriginator'),
-                        components.add_figure_card('table-DataByPlatform'),
-                    ],
-                    className="row row-cols-1 row-cols-md-2 row-cols-lg-2",
-                    ),
-                dash.html.Div(
-                    [
-                        components.add_figure_card('table-AllRawData')
-                    ],
-                    className="row row-cols-1",
-                    ),
+                    id='body-content',
+                    children=[
+                        dash.html.Div(
+                            [
+                                dash.dcc.Upload(
+                                    id='datatable-upload',
+                                    children=[dash.html.P('Drop/Upload Files')],
+                                    className='col border',
+                                    style={
+                                        'margin': '10px',
+                                        'height': '85vh',
+                                        'lineHeight': '80vh',
+                                        'textAlign': 'center',
+                                        'box-shadow': 'rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
+                                        },
+                                    multiple=True,
+                                    ),
+                            ],
+                            className='row row-cols-1',
+                            ),
+                    ]),
             ],
             className="container",
             style={
@@ -93,25 +78,79 @@ app.layout = dash.html.Div(
                 },
         )
     ],
-    className='container',
+    className='body',
     style={
-        'min-width': '100%',
-        'min-height': '100%',
-        'width': '100%',
-        'height': '100%',
         'margin': '0',
         'padding': '0',
         },
 )
 
 
+@app.callback(dash.dependencies.Output('body-content', 'children'),
+            dash.dependencies.Input('datatable-upload', 'filename'),
+            prevent_initial_call=True)
+def render_body(filename):
+    if filename is None:
+        raise dash.exceptions.PreventUpdate
+    else:
+        return dash.dcc.Tabs(
+                [
+                    dash.dcc.Tab(
+                        label='Graphs & Tables',
+                        children=[
+                            dash.html.Div(
+                                [
+                                    components.add_figure_card_half_row('piechart-CounyryPlatformOriginator'),
+                                    components.add_figure_card_half_row('table-DataByCountry'),
+                                ],
+                                className="row row-cols-1 row-cols-md-2 row-cols-lg-2",
+                                ),
+                            dash.html.Div(
+                                [
+                                    components.add_figure_card_half_row('piechart-PlatformOriginator'),
+                                    components.add_figure_card_half_row('table-DataByPlatform'),
+                                ],
+                                className="row row-cols-1 row-cols-md-2 row-cols-lg-2",
+                                ),
+                            ],
+                        style={
+                            'padding': '6px',
+                            },
+                        selected_style={
+                            'padding': '6px',
+                            }
+                        ),
+                    dash.dcc.Tab(
+                        label='Raw Data',
+                        children=[
+                            dash.html.Div(
+                                [
+                                    components.add_figure_card_full_row('table-AllRawData')
+                                ],
+                                className="row row-cols-1",
+                                ),
+                            ],
+                        style={
+                            'padding': '6px',
+                            },
+                        selected_style={
+                            'padding': '6px',
+                            }
+                        )
+                ],
+                style={
+                    'height': 'fit-content',
+                    }
+                )
+
+
 @app.callback(dash.dependencies.Output('investment-raw-data', 'data'),
               dash.dependencies.Input('datatable-upload', 'contents'),
-              dash.dependencies.State('datatable-upload', 'filename'))
+              dash.dependencies.State('datatable-upload', 'filename'),
+              prevent_initial_call=True)
 def update_output(list_of_contents, list_of_names):
-    if list_of_contents is None:
+    if list_of_names is None:
         raise dash.exceptions.PreventUpdate
-
     list_dataframes = []
     for file_name, contents in zip(list_of_names, list_of_contents):
         _, content_string = contents.split(',')
@@ -135,7 +174,8 @@ def update_output(list_of_contents, list_of_names):
               dash.dependencies.Output('piechart-CounyryPlatformOriginator', 'figure'),
               dash.dependencies.Output('table-DataByCountry', 'figure'),
               dash.dependencies.Output('table-AllRawData', 'figure'),
-              dash.dependencies.Input('investment-raw-data', 'data'))
+              dash.dependencies.Input('investment-raw-data', 'data'),
+              prevent_initial_call=True)
 def update_graphs(investment_raw_data):
     investment_raw_dataframe = pandas.read_json(investment_raw_data, orient='split')
     fig1 = charts.piechart_OriginatorCountry(investment_raw_dataframe)
